959a06d7c06487d8fffdd315a610a64c7321f6da
森田賢二 <kenjimoritata@gmail.com>
2018-01-14 13:20:02 +0900

    not, None

---
+++ hello2.py
@@ -0,0 +1,24 @@
+import tkinter
+font = ( "Helevetica", 32)
+  #""" 3time is  newline
+var = tkinter.Label(text="""
+Hello world
+ efafa
+""", font=font)
+
+#not
+#when value is false, return True
+print(not 0)
+# True
+
+#None
+x = None
+print(x is None) #return True
+
+
+
+
+var.pack()
+var.mainloop()
+
+ 
